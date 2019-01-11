987b5f91999ec147caed17bafb7596aca44904a5
Sung Kim <sungmkim80@gmail.com>
2019-01-10 19:52:58 -0500

    Fixed hard coded <a /> to use "tag"

--- src/dangerous.js
+++ src/dangerous.js
@@ -4,24 +4,28 @@ import domElements from "./domElements";
 
 class DangerousComponent extends Component {
   render() {
-    const { tag, args, forwardedRef } = this.props;
+    const { as: WrappedComponent, args, forwardedRef } = this.props;
     const [texts, ...callbacks] = args;
 
     const __html = texts
       .map((text, i) => `${text}${args[i + 1] ? callbacks[i](this.props) : ""}`)
       .reduce((unsafeText, line) => (unsafeText += line), "");
 
-    return <a ref={forwardedRef} dangerouslySetInnerHTML={{ __html }} />;
+    return (
+      <WrappedComponent
+        ref={forwardedRef}
+        dangerouslySetInnerHTML={{ __html }}
+      />
+    );
   }
 }
 
-// function dangerous(...args) {
 function constructWithOptions(tag, args) {
-  const WrappedStyledComponent = React.forwardRef((props, ref) => (
-    <DangerousComponent args={args} forwardedRef={ref} {...props} />
+  const WrappedComponent = React.forwardRef((props, ref) => (
+    <DangerousComponent as={tag} args={args} forwardedRef={ref} {...props} />
   ));
 
-  return WrappedStyledComponent;
+  return WrappedComponent;
 }
 
 const dangerous = tag => (...args) => constructWithOptions(tag, args);
