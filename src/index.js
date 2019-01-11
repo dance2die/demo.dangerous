28e5de8ae2161f2838d86e4eeae0b3c5848fc4b3
Sung Kim <sungmkim80@gmail.com>
2019-01-10 22:21:45 -0500

    Shows alert
    & Decided not to support raw...

--- src/index.js
+++ src/index.js
@@ -5,17 +5,18 @@ import dangerous from "./dangerous";
 
 import "./styles.css";
 
-const Dangerous = dangerous.xx`
+const Dangerous = dangerous.div`
   <h1>Who am I?</h1>
   <p>Last Name is "${props => props.lastName}"</p>
   <p>First Name is "${props => props.firstName}"</p>
+  <a href="javascript:alert('hi');">Show Alert</a>
 `;
 
 // console.log(`dangerous`, dangerous.div);
 function App() {
   return (
     <div className="App">
-      <Dangerous safe firstName="Sung" lastName="Kim" />
+      <Dangerous firstName="Sung" lastName="Kim" />
     </div>
   );
 }
